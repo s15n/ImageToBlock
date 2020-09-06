56af00f7eec14a8f08df913e3a42ccd6c502f759
BuildTools <unconfigured@null.spigotmc.org>
2020-09-06 17:09:33 +0200

    Updated Main.java

--- src/main/java/visar/plugins/imagetoblockplugin/Main.java
+++ src/main/java/visar/plugins/imagetoblockplugin/Main.java
@@ -23,67 +23,74 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import visar.plugins.imagetoblockplugin.RGBBlockColor;
 
-public class Main extends JavaPlugin implements Listener{
-	private static BufferedImage image = null,
-				     resizedImage = null;
-	@Override
-	public void onEnable() {
-		Bukkit.getPluginManager().registerEvents(this, this);
-		
-		try {
-		//image = ImageIO.read(new File("C:\\Users\\visar\\OneDrive\\Dokumente\\pikachu.jpg"));
-			image = ImageIO.read(new URL("https://www.github.com/FauxKiwi/ImageToBlock/tree/master/src%2Fmain%2Fresources/test.png"));
-		
-		}catch(IOException e) {
-			getServer().broadcastMessage("Didn't work");
-		}
-		
-		
-	} 
-	@EventHandler
-	public void woodAxeRightClick(PlayerInteractEvent e) {
-		Player player = e.getPlayer();
-		String path = player.getUniqueId().toString()+".locations";
+public class Main extends JavaPlugin implements Listener {
+    private static BufferedImage image = null,
+            resizedImage = null;
 
-		if(player.getInventory().getItemInMainHand().getType() == Material.WOODEN_AXE && e.getAction() == Action.LEFT_CLICK_BLOCK) {
-			Location l = e.getClickedBlock().getLocation();
-			if(this.getConfig().get(path+".firstloc") == null) {
-				this.getConfig().set(path+".firstloc",l);
-			}else this.getConfig().set(path+".secondloc",l);
-			
-			
-			player.sendMessage(image.getHeight()+" "+image.getWidth());
-			//player.sendMessage("The Z Axis is the width of the displayed picture, the X Axis is the height of the picture");
-		}
-		if(this.getConfig().get(path+".firstloc") != null && this.getConfig().get(path+".secondloc") != null) {
-			Location firstl = (Location) this.getConfig().get(path+".firstloc"),
-				 secondl = (Location) this.getConfig().get(path+".secondloc");
-						
-			int bigX = firstl.getBlockX() > secondl.getBlockX() ? firstl.getBlockX() : secondl.getBlockX(),
-			    smallX = bigX == firstl.getBlockX() ? secondl.getBlockX() : firstl.getBlockX(),
-			    bigZ = firstl.getBlockZ() > secondl.getBlockZ() ? firstl.getBlockZ() : secondl.getBlockZ(),
-			    smallZ = bigZ == firstl.getBlockZ() ? secondl.getBlockZ() : firstl.getBlockZ();
-				//resizingImage(image,(bigZ-smallZ),(bigX-smallX));
-					
-				int row = 0;
-				for(int i = smallZ; i<=bigZ; i++) {
-					int column = 0;
-					for(int j= smallX; j<=bigX; j++) {
-						Location l = new  Location(firstl.getWorld(), j, firstl.getBlockY(), i);
-						Block b = l.getBlock();
-						b.setType(RGBBlockColor.getClosestBlockValue(new Color(image.getRGB(column, row))));
-						column++;
-					}
-					row++;
-									
-			}
-			this.getConfig().set(path+".firstloc",null);
-			this.getConfig().set(path+".secondloc",null);	
-			
-		}
-	}
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+
+        try {
+            //image = ImageIO.read(new File("C:\\Users\\visar\\OneDrive\\Dokumente\\pikachu.jpg"));
+            image = ImageIO.read(new File("C:\\Users\\Simon\\Desktop\\Visar Server\\test.png"));
+
+        } catch (IOException e) {
+            getServer().broadcastMessage("Didn't work");
+        }
+
+
+    }
+
+    @EventHandler
+    public void woodAxeRightClick(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+        String path = player.getUniqueId().toString() + ".locations";
+
+        if (player.getInventory().getItemInMainHand().getType() == Material.WOODEN_AXE && e.getAction() == Action.LEFT_CLICK_BLOCK) {
+            assert e.getClickedBlock()!=null;
+            Location l = e.getClickedBlock().getLocation();
+            if (this.getConfig().get(path + ".firstLoc") == null) {
+                this.getConfig().set(path + ".firstLoc", l);
+            } else this.getConfig().set(path + ".secondLoc", l);
+
+            player.sendMessage(image.getHeight() + " " + image.getWidth());
+            //player.sendMessage("The Z Axis is the width of the displayed picture, the X Axis is the height of the picture");
+        }
+        if (this.getConfig().get(path + ".firstLoc") != null && this.getConfig().get(path + ".secondLoc") != null) {
+            Location firstL = (Location) this.getConfig().get(path + ".firstLoc"),
+                    secondL = (Location) this.getConfig().get(path + ".secondLoc");
+
+            if(firstL==null || secondL==null) {
+                player.sendMessage("§cSomething went wrong!");
+                return;
+            }
+
+            int bigX = Math.max(firstL.getBlockX(), secondL.getBlockX()),
+                    smallX = bigX == firstL.getBlockX() ? secondL.getBlockX() : firstL.getBlockX(),
+                    bigZ = Math.max(firstL.getBlockZ(), secondL.getBlockZ()),
+                    smallZ = bigZ == firstL.getBlockZ() ? secondL.getBlockZ() : firstL.getBlockZ();
+            resizingImage(image,(bigZ-smallZ),(bigX-smallX));
+
+            int row = 0;
+            for (int i = smallZ; i <= bigZ; i++) {
+                int column = 0;
+                for (int j = smallX; j <= bigX; j++) {
+                    Location l = new Location(firstL.getWorld(), j, firstL.getBlockY(), i);
+                    Block b = l.getBlock();
+                    b.setType(RGBBlockColor.getClosestBlockValue(new Color(resizedImage.getRGB(column, row))));
+                    column++;
+                }
+                row++;
+
+            }
+            this.getConfig().set(path + ".firstLoc", null);
+            this.getConfig().set(path + ".secondLoc", null);
+
+        }
+    }
 	
-	/*private static BufferedImage resizingImage(BufferedImage srcimage,int new_width, int new_height) {
+	private static BufferedImage resizingImage(BufferedImage srcimage,int new_width, int new_height) {
 		
 		resizedImage = new BufferedImage(new_width, new_height, Transparency.TRANSLUCENT);
 		Graphics2D g2 = resizedImage.createGraphics();
@@ -92,6 +99,6 @@ public class Main extends JavaPlugin implements Listener{
 		g2.dispose();
 		
 		return resizedImage;
-	}*/
-	
+	}
+
 }
