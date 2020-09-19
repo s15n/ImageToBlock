c03568a4eabc1d775f90633b5a41e011ea885299
visar77 <visarlumi77@gmail.com>
2020-09-19 19:11:05 +0200

    Optimized

--- src/main/java/visar/plugins/ImagetoBlockPlugin/ImageRenderer.java
+++ src/main/java/visar/plugins/ImagetoBlockPlugin/ImageRenderer.java
@@ -17,38 +17,20 @@ public class ImageRenderer {
         String path = p.getUniqueId().toString();
         boolean vert = plugin.getConfig().getBoolean(path+".vertical");
         BufferedImage resimg = img;
-        try {
-            if (img.getHeight() != height && img.getWidth() != width) resimg = resizingImage(img, width, height);
-        }catch(NullPointerException ignored) { }
+        if(img != null && img.getHeight() != height && img.getWidth() != width) resimg = resizingImage(img,width,height);
 
-        if(vert) {
-            l2.add(0,height,0);
-            for(int i=0; i <= width; i++) {
-                for(int j=0; j<= height; j++) {
-                    l2.add(0,-1,0).getBlock().setType(img == null ? Material.WHITE_CONCRETE : NewRGB.getClosestBlockValue(new Color(resimg.getRGB(i,j))));
-                }
-                l2.add(1,height,0);
+        for(int i=0; i < width; i++) {
+            for(int j=0; j< height; j++) {
+                l2.add(0,vert ? -1 : 0,vert ? 0 : 1).getBlock().setType(img == null ? Material.WHITE_CONCRETE : NewRGB.getClosestBlockValue(new Color(resimg.getRGB(i,j))));
             }
-            if(!plugin.getConfig().getBoolean(p.getUniqueId().toString()+".togglevertwarning")) {
-                p.sendMessage("§aImage displayed horizontally");
-                p.sendMessage("§cIf you want to display the image vertically use §6/vertical on§r");
-                p.sendMessage("§cIf you don't want to see this warning anymore use §6/togglevertwarning");
-            }
-        }else {
-            for(int i=0; i < width; i++) {
-                for(int j=0; j< height; j++) {
-                    l2.add(0,0,1).getBlock().setType(img == null ? Material.WHITE_CONCRETE : NewRGB.getClosestBlockValue(new Color(resimg.getRGB(i,j))));
-                }
-                l2.add(1,0,-height);
-            }
-            if(!plugin.getConfig().getBoolean(p.getUniqueId().toString()+".togglevertwarning")) {
-                p.sendMessage("§aImage displayed vertically");
-                p.sendMessage("§cIf you want to display the image horizontally use §6/vertical off§r");
-                p.sendMessage("§cIf you don't want to see this warning anymore use §6/togglevertwarning");
-            }
+            l2.add(1,vert ? height : 0,vert ? 0 : -height);
+        }
+        if(!plugin.getConfig().getBoolean(p.getUniqueId().toString()+".togglevertwarning")) {
+            p.sendMessage("§aImage displayed" + (vert ? "vertically":"horizontally"));
+            p.sendMessage("§cIf you want to display the image"+ (vert ? "horizontally":"vertically") + "use §6/vertical"+ (vert ? "off":"on"));
+            p.sendMessage("§cIf you don't want to see this warning anymore use §6/togglevertwarning");
         }
     }
-
 
     public static BufferedImage resizingImage(BufferedImage srcimage,int new_width, int new_height) {
 
